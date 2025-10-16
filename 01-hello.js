9ad421283b537cc5a49f2904a6de91f85e26b85a
lucas_broda <lulubrod@gmail.com>
2025-10-16 14:01:02 +0100

    feat(01-hello.js): ajout d'un premier fichier qui traite des types en js

---
+++ 01-hello.js
@@ -0,0 +1,15 @@
+let x = "Hello"
+
+x = 10
+
+const y = true
+
+const z = ["I", "Love", "JS"]
+
+function add(a, b){
+    console.log("Valeur de a :", a)
+    console.log("Valeur de b :", b)
+    return a + b
+}
+
+console.log(add(5, 10))
